9857c5e1da92286348618de80e1f1473122b7228
samwendo <wendosam21@gmail.com>
2019-09-01 23:48:36 +0300

    add javascript function

---
+++ js/script.js
@@ -0,0 +1,101 @@
+
+function Pizza(selectedSize,selectedToppings,selectedCrust) {
+    this.selectedSize = selectedSize;
+    this.selectedToppings = selectedToppings;
+    this.selectedCrust = selectedCrust;
+  }
+  
+  
+  
+  Pizza.prototype.sizeCost = function() {
+    var sizes = {
+    
+      small: 600,
+      medium: 800,
+      large: 1000
+    };
+  
+    return sizes[this.selectedSize];
+  }
+  
+  
+  
+  Pizza.prototype.toppingsCost = function() {
+    var totalToppingsCost = 0;
+    var toppings = {
+      pepperoni: 200,
+      sausage: 200,
+      bacon: 200,
+      cheese: 200,
+      onion: 200,
+    };
+  
+    this.selectedToppings.forEach(function(topping) {
+      totalToppingsCost += toppings[topping];
+    });
+  
+    return totalToppingsCost;
+  }
+  
+  
+  
+  Pizza.prototype.crustCost = function() {
+    var crusts = {
+    
+      crispy: 100,
+      stuffed: 100,
+      gluten: 100
+    };
+  
+    return crusts[this.selectedCrust];
+  }
+  
+  
+  Pizza.prototype.pizzaCost = function() {
+    return this.sizeCost() + this.toppingsCost() + this.crustCost();
+  }
+  
+  
+  
+  
+  $(document).ready(function() {
+    $('#pizza-order').submit(function(event) {
+      event.preventDefault();
+    
+      var size = $("input[name=optradio1]:checked").val();
+      var crust = $("input[name=me]:checked").val();
+      var toppings = [];
+  
+      
+      $("input:checkbox:checked").map(function(){
+        toppings.push($(this).val());
+      });
+      
+      var userPizza = new Pizza(size,toppings,crust);
+      var grandTotal = userPizza.pizzaCost().toFixed(2);
+  
+      $('#totalCostSpan').text(grandTotal);
+  
+  
+    });
+     $('.but').click(function() {
+      $('.second').show();
+      
+    
+  
+  
+     });
+  
+     $('.bu').click(function() {
+      alert("your Order will be ready in 30 mints,Thank you And Welcome Once Again ")
+      
+     
+      
+    
+  
+  
+     });
+   
+  
+  });
+  
